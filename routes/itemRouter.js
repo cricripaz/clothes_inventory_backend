6111ff6efee8a98abe082d6521679e399f8e2307
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-06 16:19:15 -0400

    modify post service model name Item for item 2

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const itemController = require('../controllers/userController')
+const itemController = require('../controllers/itemController')
 const router = express.Router();
 
 
