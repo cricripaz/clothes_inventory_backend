432d9d82a6e46e1d8bd2dd8b659ff17ba5903bc4
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-06 16:02:49 -0400

    modify post service

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const _connect = require('./db/_connect');
 
 
@@ -16,13 +15,5 @@ const app = express();
 app.use(bodyParser.json());
 
 
-
-
-
-//ROutes
-
-app.get('/',(req,res) => {
-    res.send({message: "hola v"})
-})
 
 app.listen(process.env.PORT, () => console.log('server on port', process.env.PORT))
