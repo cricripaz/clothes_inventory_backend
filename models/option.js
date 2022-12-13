b8cb5702194e8d55759b76ea8e001de5e8e6ad35
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-12 20:57:13 -0400

    add model option

---
+++ models/option.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const e = require("express");
+
+
+const optionSchema = mongoose.Schema({
+
+    name : {
+        type:String,
+        required: true
+    }
+})
+
+
+optionSchema.statics.addOptionType = addOptionType;
+optionSchema.statics.getOptionsType = getOptionsType;
+
+
+
+module.exports = mongoose.model('option','options-type')
+
+
+
+function getOptionsType(){
+    return this.find({});
+}
+
+
+function addOptionType(option_info){
+
+    const newOption = {
+        name:option_info.name
+    }
+
+    return this.create(newOption);
+
+}
