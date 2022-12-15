a665c594b179bce204e3bf9771e2e81a889c6405
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-15 17:31:51 -0400

    Service GET item-by-name

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/addItem', itemController.addItem);
 router.get('/getAll',itemController.getAllItems);
 router.get('/getItem',itemController.getItem);
-router.get('/getItemByName',itemController.getItemByName);
+router.post('/getItemByName',itemController.getItemByName);
 router.put('/addQuantityProduct',itemController.addQuantity);
 
 module.exports = router;
