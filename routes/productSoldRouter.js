3445b1392f4aa01c65c276b8f09084ff5ecc2dd5
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-20 01:58:30 -0400

    Add Service Sell Products FIX

--- routes/productSoldRouter.js
+++ routes/productSoldRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const productSoldController = require('../controllers/productSoldController');
-const getModelByName = require("../db/getModelByName");
 const router = express.Router();
 
 router.post('/products-sold',productSoldController.addSellProduct);
