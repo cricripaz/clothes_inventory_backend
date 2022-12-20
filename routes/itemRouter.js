5ebdefe24c463f40f1150c4a255acc1bb203ad7b
cricripaz <49013118+cricripaz@users.noreply.github.com>
2022-12-20 00:36:15 -0400

    Add Service GET Item By name and Type FIX

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -8,7 +8,7 @@ router.post('/addItem', itemController.addItem);
 router.get('/getAll',itemController.getAllItems);
 router.get('/getItem',itemController.getItem);
 router.post('/getItemByName',itemController.getItemByName);
-router.post('/get-item-by-name-type',getItemByNameType);
+router.post('/get-item-by-name-type',itemController.getItemByNameType);
 router.post('/getQuantity',itemController.getQuantity);
 router.post('/get-quantity-for-sale',itemController.getQuantityForSale);
 router.put('/addQuantityProduct',itemController.addQuantity);
